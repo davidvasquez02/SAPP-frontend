520c5be8c75c6e76624d61a16b99eec5691e0653
David Vasquez <davidvasquez_02@outlook.es>
2026-03-31 18:24:55 -0500

    ajuste target sapp

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'https://uisrooms.eisi.online',
+        target: 'https://sapp.eisi.online',
         changeOrigin: true,
         secure: false,
       },
