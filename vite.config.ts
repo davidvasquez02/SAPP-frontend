f4aede13003c98b8f6d1dafb7530fe10432b3384
David Vasquez <davidvasquez_02@outlook.es>
2026-03-31 18:23:53 -0500

    ajustes ci cd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://uisrooms.eisi.online',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    outDir: 'build',
+  }
+});
