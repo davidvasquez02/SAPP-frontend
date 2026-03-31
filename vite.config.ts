496e5d7f4da6df7ce3ec42db4e77b4b24668abcc
David Vasquez <davidvasquez_02@outlook.es>
2026-03-31 18:29:43 -0500

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'https://sapp.eisi.online',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-  build: {
-    outDir: 'build',
-  }
-});
+})
